c7155a87311db2666deefba445628b587b9e394e
Paul Santos <paulmarlonsantos@gmail.com>
2020-08-27 02:37:39 +0800

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -19323,17 +19323,6 @@ module.exports = function(module) {
 
 /***/ }),
 
-/***/ "./resources/css/main.css":
-/*!********************************!*\
-  !*** ./resources/css/main.css ***!
-  \********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -19377,15 +19366,26 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/css/main.css ***!
-  \************************************************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Users/paulmarlonsantos/Sites/munchbox/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/paulmarlonsantos/Sites/munchbox/resources/css/main.css */"./resources/css/main.css");
+module.exports = __webpack_require__(/*! /Users/paulmarlonsantos/Sites/munchbox/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
