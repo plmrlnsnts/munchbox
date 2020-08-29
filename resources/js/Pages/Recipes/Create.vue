642fc99c9cc4e65ccfe51b8e12dc63678598cf2a
Paul Santos <paulmarlonsantos@gmail.com>
2020-08-29 14:27:24 +0800

    wip

--- resources/js/Pages/Recipes/Create.vue
+++ resources/js/Pages/Recipes/Create.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="flex flex-col h-screen">
-        <div class="h-12 bg-indigo-500 flex justify-between">
-            <InertiaLink :href="$route('home')" class="inline-flex items-center px-4 text-white font-semibold text-sm leading-5">
+    <div class="flex flex-col">
+        <div class="h-12 bg-gray-900 flex justify-between">
+            <InertiaLink :href="$route('home')" class="inline-flex items-center px-4 text-white font-semibold sm:text-sm sm:leading-5">
                 Cancel
             </InertiaLink>
-            <button class="inline-flex items-center px-4 text-white font-semibold text-sm leading-5 opacity-50 focus:outline-none">
+            <button class="inline-flex items-center px-4 text-white font-semibold sm:text-sm sm:leading-5 opacity-50 focus:outline-none">
                 Save
             </button>
         </div>
         <div class="flex-1 overflow-y-auto">
             <div class="p-4 space-y-4">
-                <h2 class="font-medium text-center">Let your recipe be known</h2>
+                <h2 class="font-medium text-center">Post a recipe</h2>
                 <form>
-                    <div class="space-y-4">
+                    <div class="space-y-6">
                         <FileUpload v-model="form.thumb" />
                         <div class="space-y-1">
-                            <label class="text-sm leading-5 font-medium text-gray-700">Name</label>
-                            <input type="text" class="px-3 py-2 text-sm leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" />
+                            <label class="sm:text-sm sm:leading-5 font-medium text-gray-700">Name</label>
+                            <input type="text" class="px-3 py-2 sm:text-sm sm:leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" />
                         </div>
                         <div class="space-y-1">
-                            <label class="text-sm leading-5 font-medium text-gray-700">Tags</label>
-                            <input type="text" placeholder="Maximum of 5 tags" class="px-3 py-2 text-sm leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" />
+                            <label class="sm:text-sm sm:leading-5 font-medium text-gray-700">Tags</label>
+                            <input type="text" placeholder="Maximum of 5 tags" class="px-3 py-2 sm:text-sm sm:leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" />
                             <div class="flex flex-wrap text-xs leading-5">
                                 <div class="font-medium text-pink-500 mr-2">Suggestions:</div>
                                 <span v-for="(tag, i) in ['Easy Recipe', 'Sweets', 'Beverage']" :key="`tags-${tag}`" class="text-gray-700 mr-1">
@@ -29,20 +29,20 @@
                             </div>
                         </div>
                         <div class="space-y-1">
-                            <label class="text-sm leading-5 font-medium text-gray-700">Description</label>
-                            <textarea placeholder="A quick summary for this recipe" class="px-3 py-2 text-sm leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" rows="4"></textarea>
+                            <label class="sm:text-sm sm:leading-5 font-medium text-gray-700">Description</label>
+                            <textarea placeholder="A quick summary for this recipe" class="px-3 py-2 sm:text-sm sm:leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" rows="4"></textarea>
                         </div>
                         <hr class="-mx-4">
                         <IngredientList v-model="form.ingredients" />
                         <hr class="-mx-4">
                         <div class="space-y-1">
-                            <label class="text-sm leading-5 font-medium text-gray-700">Cooking Instructions</label>
-                            <textarea placeholder="Steps for making this awesome recipe" class="px-3 py-2 text-sm leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" rows="8"></textarea>
+                            <label class="sm:text-sm sm:leading-5 font-medium text-gray-700">Cooking Instructions</label>
+                            <textarea placeholder="Steps for making this awesome recipe" class="px-3 py-2 sm:text-sm sm:leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" rows="8"></textarea>
                         </div>
                         <div class="space-y-1">
-                            <label class="text-sm leading-5 font-medium text-gray-700">Video Demo (optional)</label>
+                            <label class="sm:text-sm sm:leading-5 font-medium text-gray-700">Video Demo (optional)</label>
                             <div class="relative">
-                                <input type="text" placeholder="Youtube embed url" class="pl-10 pr-3 py-2 text-sm leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" />
+                                <input type="text" placeholder="Youtube embed url" class="pl-10 pr-3 py-2 sm:text-sm sm:leading-5 rounded-lg block w-full bg-gray-200 border border-gray-200 focus:bg-white focus:border-blue-400 focus:outline-none focus:shadow-outline transition-shadow duration-100 ease-linear" />
                                 <div class="pointer-events-none flex items-center absolute inset-0 px-3">
                                     <svg viewBox="0 0 20 20" fill="currentColor" class="text-gray-400 w-5 h-5">
                                         <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
@@ -65,6 +65,10 @@ export default {
     components: {
         FileUpload,
         IngredientList,
+    },
+
+    mounted () {
+        this.$el.style.height = `${window.innerHeight}px`
     },
 
     data: vm => ({
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="The way to build modern laravel applications.">
     <meta property="og:image" content="{{ asset('banner.jpg') }}">
     <meta property="og:title" content="Oh baby, you're gonna love this!" />
-    <meta property="og:description" content="The way to build modern laravel applications using the best tools out there." />
+    <meta property="og:description" content="You're in for a ride! We'll build laravel applications using the best tools out there." />
     <title>Munchbox</title>
     <link href="https://fonts.googleapis.com/css2?family=Chewy&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/main.css') }}">
